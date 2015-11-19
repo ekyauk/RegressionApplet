a5a43d299884b68996b95b66a93aa88103db673c
Edric Kyauk <edric.kyauk@gmail.com>
2015-11-18 17:15:41 -0800

    Adds functionality to draw a line.

--- linear_relationships_scatterplot_files/linear_relationships_scatterplot.js
+++ linear_relationships_scatterplot_files/linear_relationships_scatterplot.js
@@ -1,35 +1,33 @@
 $(document).ready(function(){
 
-
   function sign(x) { return x > 0 ? 1 : x < 0 ? -1 : 0; };
   //creates the data points distributed somewhat-evenly throughout the graph
+  var data = [];
 
-var data = [];
+  var coefficient = function(points) {
+    sumX = 0.0;
+    sumY = 0.0;
+    sumSqX = 0.0;
+    sumSqY = 0.0;
+    sumXY = 0.0;
+    n = points.length;
+    if (data.length <= 0) return 0;
+    for (var i = 0; i < points.length; i++) {
+      var point = points[i];
+      sumX += point[0];
+      sumY += point[1];
+      sumSqX += Math.pow(point[0], 2);
+      sumSqY += Math.pow(point[1], 2);
+      sumXY += point[0] * point[1];
 
-var coefficient = function(points) {
-  sumX = 0.0;
-  sumY = 0.0;
-  sumSqX = 0.0;
-  sumSqY = 0.0;
-  sumXY = 0.0;
-  n = points.length;
-  if (data.length <= 0) return 0;
-  for (var i = 0; i < points.length; i++) {
-    var point = points[i];
-    sumX += point[0];
-    sumY += point[1];
-    sumSqX += Math.pow(point[0], 2);
-    sumSqY += Math.pow(point[1], 2);
-    sumXY += point[0] * point[1];
+    }
 
+      num = sumXY * n - sumX * sumY;
+      denom = Math.pow(sumSqX * n - Math.pow(sumX,2), 0.5) * Math.pow(n * sumSqY - Math.pow(sumY, 2), 0.5);
+      if (denom == 1) return 1;
+      r = num/denom;
+      return r;
   }
-
-    num = sumXY * n - sumX * sumY;
-    denom = Math.pow(sumSqX * n - Math.pow(sumX,2), 0.5) * Math.pow(n * sumSqY - Math.pow(sumY, 2), 0.5);
-    if (denom == 1) return 1;
-    r = num/denom;
-    return r;
-}
 
   var margin = {top: 20, right: 15, bottom: 60, left: 60}
     , width = 500 - margin.left - margin.right
@@ -90,57 +88,54 @@ var coefficient = function(points) {
           .attr("r", 4);
   }
 
-
-  //moves the datapoints towards the line y=x or y=-x when the slider moves.
   $('body').click(function(e){
-    var x = (e.pageX - ($('#chart').position().left + margin.left))/width * 100;
-    var y = ($('#chart').position().top + height + margin.top - e.pageY)/height * 100;
-    if (x < 0 || x > 100 || y < 0 || y > 100) return;
-    addCircle([x, y]);
-    $('#r-result').text(coefficient(data));
+    if ($('#addPoint').is(":checked")) {
+      var px = (e.pageX - ($('#chart').position().left + margin.left))/width * 100;
+      var py = ($('#chart').position().top + height + margin.top - e.pageY)/height * 100;
+      console.log("Clicked at " + px + " ," + py);
+      if (px < 0 || px > 100 || py < 0 || py > 100) return;
+      addCircle([px, py]);
+      $('#r-result').text("R: " + coefficient(data));
+    }
+  });
+
+  var lineStartX = 0;
+  var lineStartY = 0;
+  var isDrawingLine = false;
+  $('body').mousedown(function(e){
+    if (!$('#addPoint').is(":checked")&& $('#drawn-line').length == 0) {
+      isDrawingLine = true;
+      lineStartX = (e.pageX - ($('#chart').position().left + margin.left))/width * 100;
+      lineStartY = ($('#chart').position().top + height + margin.top - e.pageY)/height * 100;
+      console.log("Starting line at " + lineStartX + " ," + lineStartY);
+      console.log("start drawing");
+    }
+  });
+  $('body').mousemove(function(e){
+    if (!$('#addPoint').is(":checked") && isDrawingLine) {
+      $('#drawn-line').remove();
+      lineEndX = (e.pageX - ($('#chart').position().left + margin.left))/width * 100;
+      lineEndY = ($('#chart').position().top + height + margin.top - e.pageY)/height * 100;
+      g.append("line")
+        .attr('id', 'drawn-line')
+        .attr("x1", x(lineStartX))
+        .attr("y1", y(lineStartY))
+        .attr("x2", x(lineEndX))
+        .attr("y2", y(lineEndY))
+        .attr("stroke-width", 2)
+        .attr("stroke", "green")
+    }
+  });
+
+  $('body').mouseup(function(){
+    if (isDrawingLine) {
+      isDrawingLine = false;
+    }
   });
 
   $('#clear').click(function(){
     data = [];
     g.selectAll("*").remove();
   });
-// content.click(function(e){
-//   var x = chart.position().left - e.pageX;
-//   var y = chart.position().bottom - e.pageY;
-//   var marker;
-//   if(numbersign >= 0){
-//     circles.attr("cy", function (d) { return y(d[1]-numbersign*( (d[1]-d[0])*document.getElementById("slider").value)); } );
-//     d3.select("#r").style("color","black");
-//   }
-//   else{
-//     circles.attr("cy", function (d) { return y(d[1]+numbersign*( (100-d[0]-d[1])*document.getElementById("slider").value)); } );
-//     d3.select("#r").style("color","red");
-//   }
-//   d3.select("#r").text("r = " + document.getElementById("slider").value);
-
-// })
-
-//reverses the sign when them button is clicked
-var button = d3.select("#button");
-button.on("click",function(){
-  document.getElementById("slider").value = -document.getElementById("slider").value;
-  var numbersign =  sign(document.getElementById("slider").value);
-  var marker;
-  if(numbersign >= 0){
-    circles.attr("cy", function (d) { return y(d[1]-numbersign*( (d[1]-d[0])*document.getElementById("slider").value)); } );
-    d3.select("#r").style("color","black");
-  }
-  else{
-    circles.attr("cy", function (d) { return y(d[1]+numbersign*( (100-d[0]-d[1])*document.getElementById("slider").value)); } );
-    d3.select("#r").style("color","red");
-  }
-  d3.select("#r").text("r = " + document.getElementById("slider").value);
-
 
 })
-
-
-
-
-
-        })
